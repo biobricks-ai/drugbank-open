7065e020d60fabfb0e64456ad00c548447cec69a
Sulstice <sharifsuliman1@gmail.com>
2024-07-17 13:40:51 -0500

    Added the infrastructure to drugbank

--- stages/01_download_data.sh
+++ stages/01_download_data.sh
@@ -2,4 +2,4 @@
 
 mkdir -p ./download
 
-wget -P download/ https://go.drugbank.com/releases/5-1-12/downloads/all-drugbank-vocabulary
+wget -P download/ -O all-drugbank-vocabulary.zip https://go.drugbank.com/releases/5-1-12/downloads/all-drugbank-vocabulary
