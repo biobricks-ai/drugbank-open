f074aefedafb8bd2164b08c4e36558172bebd2a4
sulstice <sharifsuliman1@gmail.com>
2024-07-18 18:48:38 +0000

    Modified the bug in the zip file to read_zip

--- stages/02_process_data.py
+++ stages/02_process_data.py
@@ -18,15 +18,9 @@ for file in files:
   out_basename = re.sub(extensions_re, '.parquet', file.name )
   out_file = brick_dir / file.relative_to('download').with_name( out_basename )
 
-  file = str(file)
-
-  if file.endswith('.gz'):
-    with gzip.open(file, 'rb') as file_in:
-      lines = file_in.read().decode("utf-8").split('\n')
-      data = [ line.split('\t') for line in lines ]
-      headers = data.pop(0)
-      df = pd.DataFrame(data, columns=headers)
-      df.to_parquet(out_file)
+  if file.match('*.zip'):
+    df = pd.read_csv(file, compression='zip', header=0, sep=',', quotechar='"')  
+    df.to_parquet(out_file)
 
   else:
     raise Exception('Unknown File Found: %s' % file)
